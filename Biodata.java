afc22ffe6b456de85a67fa049975a67076a939b4
virginyosse <virginyosse01@gmail.com>
2021-09-26 14:48:59 +0700

    first commit

---
+++ Biodata.java
@@ -0,0 +1,8 @@
+public class Biodata{
+
+	public static void main (String[] args) {
+		System.out.println ("Nama : Virgin Yosse Beatrik Depari");
+		System.out.println ("Nim  : 6706213117");
+		System.out.println ("Kelas: D3 RPLA 45 04");
+	}
+}
