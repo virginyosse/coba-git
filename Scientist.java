e654ec5df8341246bd7c93a6bb9c71a62dd43019
virginyosse <virginyosse01@gmail.com>
2021-10-02 19:07:16 +0700

    lagu

---
+++ Scientist.java
@@ -0,0 +1,8 @@
+public class Scientist{
+
+	public static void main (String[] args) {
+		System.out.println ("come up to meet u");
+		System.out.println ("tell u im sorry");
+		System.out.println ("u dont know how lovwly u are");
+	}
+}
